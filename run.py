9019f13551801b6c754fd6f872409f3ab2bb2b85
cmadajski <cmad7317@tutanota.com>
2023-05-27 13:45:09 -0400

    Read config data from yaml

--- run.py
+++ run.py
@@ -1,21 +1,26 @@
 import google.generativeai as palm
 import os
+import yaml
+
+def read_yaml(yamlFile: str) -> dict:
+    with open(yamlFile, 'r') as f:
+        yamlDict = yaml.safe_load(f)
+    return yamlDict
 
 if __name__ == "__main__":
     # get PaLM API key from environment variable
     palm.configure(api_key=os.getenv('palm_key'))
-    # get text generation models
+    # read config data from yaml
+    config = read_yaml('config.yaml')
+    # to get text generation models
     models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
     model = models[0].name
-    # set reasonable defaults for temp and max_output_tokens
-    temp = 0
-    max_token = 800
     # get user prompt
     prompt = input('Enter a prompt: ')
     # send data to the PaLM API if all required data is present
     if prompt:
         # send query data to PaLM API
-        completion = palm.generate_text(model=model, prompt=prompt, temperature=temp, max_output_tokens=max_token)
+        completion = palm.generate_text(model=model, prompt=prompt, temperature=config['temp'], max_output_tokens=config['max_output_tokens'])
         # print API response
         print(completion.result)
     # print an error message if prompt is empty
