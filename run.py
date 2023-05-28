4f8413bab78dfa6da93b321882875cbfb0167712
cmadajski <cmad7317@tutanota.com>
2023-05-27 22:44:22 -0400

    Add chat functionality

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import google.generativeai as palm
 import os
 import yaml
 
+# app config
+valid_text_input = ['text', 't', 'txt']
+valid_chat_input = ['chat', 'c', 'ch']
+valid_chat_exit = ['exit', 'Exit', 'q', 'quit', 'Q', 'Quit']
+
 def read_yaml(yamlFile: str) -> dict:
     with open(yamlFile, 'r') as f:
         yamlDict = yaml.safe_load(f)
@@ -12,17 +17,46 @@ if __name__ == "__main__":
     palm.configure(api_key=os.getenv('palm_key'))
     # read config data from yaml
     config = read_yaml('config.yaml')
-    # to get text generation models
-    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-    model = models[0].name
-    # get user prompt
-    prompt = input('Enter a prompt: ')
-    # send data to the PaLM API if all required data is present
-    if prompt:
-        # send query data to PaLM API
-        completion = palm.generate_text(model=model, prompt=prompt, temperature=config['temp'], max_output_tokens=config['max_output_tokens'])
-        # print API response
-        print(completion.result)
-    # print an error message if prompt is empty
+    # determine which generation method is desired
+    generation_method = input("Choose a generation method (text or chat): ")
+    # TEXT GENERATION
+    if generation_method in valid_text_input:
+        # to get text generation models
+        models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+        model = models[0].name
+        # get user prompt
+        prompt = input('Enter a prompt: ')
+        # send data to the PaLM API if all required data is present
+        if prompt:
+            # send query data to PaLM API
+            completion = palm.generate_text(model=model, prompt=prompt, temperature=config['temp'], max_output_tokens=config['max_output_tokens'])
+            # print API response
+            print(completion.result)
+        # print an error message if prompt is empty
+        else:
+            print("No prompt provided. A prompt is required to use the PaLM API.")
+    # CHAT GENERATION
+    elif generation_method in valid_chat_input:
+        continue_chat = True
+        # start chat conversation
+        chat_start_message = input('Start conversation: ')
+        if chat_start_message in valid_chat_exit:
+            continue_chat = False
+        elif not chat_start_message: 
+            print('Prompt is empty. Use "quit" or "exit" to end chat.')
+        else:
+            response = palm.chat(messages=chat_start_message)
+            print(f'PaLM: {response.last}')
+            while continue_chat:
+                chat_reply_message = input('Your reply: ')
+                if chat_reply_message in valid_chat_exit:
+                    continue_chat = False
+                elif not chat_reply_message:
+                    print('Prompt is empty. Use "quit" or "exit" to end chat.')
+                else:
+                    response = response.reply(chat_reply_message)
+                    print(f'PaLM: {response.last}')
+    # GENERATION NOT SUPPORTED
     else:
-        print("No prompt provided. A prompt is required to use the PaLM API.")
+        print(f'Generation method "{generation_method}" is not supported.')
+
